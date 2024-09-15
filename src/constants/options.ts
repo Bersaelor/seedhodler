2163fe43d754ca071763c18d4f42856f95e626e8
Konrad Feiler <konrad@mathheartcode.com>
2024-09-15 12:39:37 +0200

    add some more language options for testing

--- src/constants/options.ts
+++ src/constants/options.ts
@@ -1,4 +1,11 @@
-export const langOptions = [{ value: "english", label: "English" }]
+export const langOptions = [
+  { value: "english", label: "English" },
+  { value: "czech", label: "Czech" },
+  { value: "chinese_simplified", label: "Simplified Chinese" },
+  { value: "chinese_traditional", label: "Traditional Chinesse" },
+  { value: "korean", label: "Korean" },
+  { value: "french", label: "French" },
+]
 
 export const wordCountOptions = [
   { value: "12", label: "12" },
